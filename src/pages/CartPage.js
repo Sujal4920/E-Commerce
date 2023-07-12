cb125cdac76f364aaacf805d30e44d3117361ff9
Sujal4920 <sujalsharma27may2000@gmail.com>
2023-07-12 16:37:14 +0530

    made cart

---
+++ src/pages/CartPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Cart } from './../features/cart/Cart';
+import NavBar from '../features/navbar/Navbar';
+
+export default function CartPage() {
+  return (
+    <div>
+        <NavBar/>
+        <Cart/>
+    </div>
+  )
+}
